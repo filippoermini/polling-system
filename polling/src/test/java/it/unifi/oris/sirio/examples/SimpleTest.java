5d27db6e05d5fef0083b0b746a92ab2864f218a2
Marco Paolieri <paolieri@gmail.com>
2016-02-12 15:46:09 +0100

    initial commit

---
+++ polling/src/test/java/it/unifi/oris/sirio/examples/SimpleTest.java
@@ -0,0 +1,13 @@
+package it.unifi.oris.sirio.examples;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SimpleTest {
+    
+    @Test
+    public void simple() {
+        assertEquals(2, 2);
+    }
+}
